3d4211a350280abc29a8fa33cdee8a66a6111372
SergioSoliz2017 <201801656@est.umss.edu>
2023-10-16 18:59:49 -0400

    UnionTodo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from './pages/LoginPage';
+import IndexPage from './pages/IndexPage';
+import MenuVertical from './pages/MenuVertical';
+import AsignacionComite from './pages/AsignacionComite'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<IndexPage/>}></Route>
+        <Route path="/login" element={<LoginPage/>}></Route>
+        <Route path="/home" element={<MenuVertical/>}></Route>
+        <Route path='/comiteElectoral' element={<AsignacionComite/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
